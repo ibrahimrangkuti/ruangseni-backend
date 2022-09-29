b25dc6f250aed18b12b22c616bc72ebd171af8c5
LuqmanSyah <eno.luqman@gmail.com>
2022-09-29 08:39:01 +0700

    Menambah aset dashboard user dan admin

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\User\PagesController as UserPagesController;
 
 // Auth
 Route::get('login', [LoginController::class, 'index'])->name('login.index');
@@ -11,3 +12,7 @@ Route::get('/', [PagesController::class, 'home'])->name('home');
 Route::get('karya', [PagesController::class, 'karya'])->name('karya');
 Route::get('event', [PagesController::class, 'event'])->name('event');
 Route::get('leaderboard', [PagesController::class, 'leaderboard'])->name('leaderboard');
+
+Route::prefix('user/')->name('user.')->group(function() {
+    // Route::get('dashboard', )
+});
