ccd63ccb1e175d7611cd91bec13f0ae533040b98
ibrahimrangkuti <rangkutyibrahim765@gmail.com>
2022-10-01 05:53:10 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,3 @@ Route::get('/', [PagesController::class, 'home'])->name('home');
 Route::get('karya', [PagesController::class, 'karya'])->name('karya');
 Route::get('event', [PagesController::class, 'event'])->name('event');
 Route::get('leaderboard', [PagesController::class, 'leaderboard'])->name('leaderboard');
-
-Route::prefix('user/')->name('user.')->group(function() {
-    // Route::get('dashboard', )
-});
