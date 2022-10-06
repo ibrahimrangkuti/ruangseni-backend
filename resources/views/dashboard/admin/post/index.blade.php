f87342e156bc93afe9552d97470eb21ba28e40ed
ibrahimrangkuti <rangkutyibrahim765@gmail.com>
2022-10-06 15:22:39 +0700

    Membuat Section Header Di Halaman Data Postingan

--- resources/views/dashboard/admin/post/index.blade.php
+++ resources/views/dashboard/admin/post/index.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    
+<section class="section">
+    <div class="section-header">
+        <h1>Data Postingan</h1>
+    </div>
+</section>
 @endsection
