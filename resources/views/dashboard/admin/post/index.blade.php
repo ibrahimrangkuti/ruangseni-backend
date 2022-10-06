d8108e1872ef6f078a2f23f7db4d025f0d15596b
LuqmanSyah <eno.luqman@gmail.com>
2022-10-06 15:49:52 +0700

    Membuat Table Data Postingan

--- resources/views/dashboard/admin/post/index.blade.php
+++ resources/views/dashboard/admin/post/index.blade.php
@@ -5,5 +5,71 @@
     <div class="section-header">
         <h1>Data Postingan</h1>
     </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-striped table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th>NO.</th>
+                                    <th>Gambar</th>
+                                    <th>Link</th>
+                                    <th>Nama Pembuat</th>
+                                    <th>Kategori</th>
+                                    <th>Judul</th>
+                                    <th>Status</th>
+                                    <th>Tanggal Diposting</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>gajah.jpg</td>
+                                <td><a href="https::google.com">https::google.com</a></td>
+                                <td>Ibrahim</td>
+                                <td>Cerpen</td>
+                                <td><a href="https::google.com">Impian Keabadian</a></td>
+                                <td>Sudah disetujui</td>
+                                <td>06-10-22</td>
+                                <td>
+                                    <a href="" class="btn btn-danger btn-sm">Hapus</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>harimau.jpg</td>
+                                <td><a href="https::google.com">https::google.com</a></td>
+                                <td>Luqman</td>
+                                <td>Seni Lukis</td>
+                                <td><a href="https::google.com">Senja</a></td>
+                                <td>Sudah disetujui</td>
+                                <td>06-11-22</td>
+                                <td>
+                                    <a href="" class="btn btn-danger btn-sm">Hapus</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>kucing.jpg</td>
+                                <td><a href="https::google.com">https::google.com</a></td>
+                                <td>Akrom</td>
+                                <td>Puisi</td>
+                                <td><a href="https::google.com">Aku adalah aku</a></td>
+                                <td>Sudah disetujui</td>
+                                <td>06-15-22</td>
+                                <td>
+                                    <a href="" class="btn btn-danger btn-sm">Hapus</a>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 @endsection
