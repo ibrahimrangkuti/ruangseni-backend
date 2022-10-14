f8b3b7f71c9a841d09fa5f96f7999e13e4e980fa
ibrahimrangkuti <rangkutyibrahim765@gmail.com>
2022-10-14 13:41:47 +0700

    Login Status User

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 use Auth;
 
 class LoginController extends Controller
@@ -15,12 +16,18 @@ class LoginController extends Controller
     public function process(Request $request)
     {
         if(Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = User::find(Auth::id());
+            $user->logged_in = true;
+            $user->update();
             if(Auth::user()->role == 'siswa') {
                 return redirect(route('user.dashboard'));
             } else {
                 return redirect(route('admin.dashboard'));
             }
         } else if(Auth::attempt(['nis' => $request->email, 'password' => $request->password])) {
+            $user = User::find(Auth::id());
+            $user->logged_in = true;
+            $user->update();
             return redirect(route('user.dashboard'));
         } else {
             return redirect(route('login.index'))->with('error', 'Data akun tidak sesuai!');
