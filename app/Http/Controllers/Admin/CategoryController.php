5f15e9d9234a912ffbb389153d5cdfb8b965176f
LuqmanSyah <eno.luqman@gmail.com>
2022-10-21 16:22:25 +0700

    Membuat view kategori

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
     public function index()
     {
-        return view('dashboard.admin.category.index');
+        $categories = Category::all();
+
+        return view('dashboard.admin.category.index', compact('categories'));
     }
+
 }
