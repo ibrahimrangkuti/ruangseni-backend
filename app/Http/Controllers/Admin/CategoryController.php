a5809d8aabcf0da862800f811bea41cb6ae8b710
LuqmanSyah <eno.luqman@gmail.com>
2022-10-28 14:58:06 +0700

    Membuat function delete di CategoryController

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 
 class CategoryController extends Controller
 {
@@ -15,4 +16,11 @@ class CategoryController extends Controller
         return view('dashboard.admin.category.index', compact('categories'));
     }
 
+    public function delete($id)
+    {
+        $category = Category::findOrFail($id);
+        $category->delete();
+
+        return redirect(route('admin.category.index'));
+    }
 }
