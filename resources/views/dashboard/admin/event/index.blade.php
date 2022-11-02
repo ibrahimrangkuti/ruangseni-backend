f27c9e98eef75cf5798368d05fade6761c0bd261
LuqmanSyah <eno.luqman@gmail.com>
2022-11-02 15:54:09 +0700

    Membuat halaman data event dashboard admin

--- resources/views/dashboard/admin/event/index.blade.php
+++ resources/views/dashboard/admin/event/index.blade.php
@@ -5,5 +5,44 @@
     <div class="section-header">
         <h1>Data Event</h1>
     </div>
+    <a href="" class="btn btn-primary mb-3">Tambah Kategori</a>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-body">
+                    @include('components.alert')
+                    <div class="table-responsive">
+                        <table class="table table-striped table-bordered table-hover" height="400px">
+                            <thead>
+                                <tr>
+                                    <th>No.</th>
+                                    <th>Thumbnail</th>
+                                    <th>Judul</th>
+                                    <th>Deskripsi</th>
+                                    <th>Waktu Awal</th>
+                                    <th>Waktu Akhir</th>
+                                    <th width="160px">Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>
+                                        <a href="" class="btn btn-warning btn-sm">Edit</a>
+                                        <a href="" class="btn btn-danger btn-sm">Hapus</a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 @endsection
