ce19cafd96ca0156e9a3a25ca73009e4175282e2
ibrahimrangkuti <rangkutyibrahim765@gmail.com>
2022-11-07 22:34:34 +0700

    Menghapus pagination di halaman karya

--- resources/views/pages/karya.blade.php
+++ resources/views/pages/karya.blade.php
@@ -49,7 +49,7 @@
             </div>
             @endforeach
 
-            <nav class="mt-5" aria-label="Page navigation example" data-aos="fade-up" data-aos-delay="100">
+            {{-- <nav class="mt-5" aria-label="Page navigation example" data-aos="fade-up" data-aos-delay="100">
                 <ul class="custom-pagination pagination">
                     <li class="page-item prev"><a class="page-link" href="#">Previous</a></li>
                     <li class="page-item active"><a class="page-link" href="#">1</a></li>
@@ -57,7 +57,7 @@
                     <li class="page-item"><a class="page-link" href="#">3</a></li>
                     <li class="page-item next"><a class="page-link" href="#">Next</a></li>
                 </ul>
-            </nav>
+            </nav> --}}
         </div>
     </div>
 </div>
