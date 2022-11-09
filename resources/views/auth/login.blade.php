9dd7707b578270827a69a32c1ba100cd401f77ab
ibrahimrangkuti <rangkutyibrahim765@gmail.com>
2022-11-10 06:13:49 +0700

    Menambahkan background di halaman login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,6 +18,15 @@
     <!-- Template CSS -->
     <link rel="stylesheet" href="{{ asset('dashboard/assets/css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('dashboard/assets/css/components.css') }}">
+    <style>
+        body {
+            background: url({{ asset('assets/images/hero_4.jpg') }});
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            height: 100%;
+        }
+    </style>
 </head>
 
 <body>
