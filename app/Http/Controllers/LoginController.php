4f0e1930c40602c80659852d9f6888917736fce7
LuqmanSyah <eno.luqman@gmail.com>
2022-11-13 07:52:45 +0700

    Membuat logic login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -10,7 +10,7 @@ class LoginController extends Controller
 {
     public function index()
     {
-        if(Auth::check()) {
+        if (Auth::check()) {
             return back();
         }
         return view('auth.login');
