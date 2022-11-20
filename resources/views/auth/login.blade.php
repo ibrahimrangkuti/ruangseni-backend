9eaa46ea643308664bb96f26ec0183d9d16f52cd
ibrahimrangkuti <rangkutyibrahim765@gmail.com>
2022-11-20 11:20:00 +0700

    memperbaiki error login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -48,12 +48,12 @@
 
                             <div class="card-body">
                                 @include('components.alert')
-                                <form method="POST" action="{{ route('login.process') }}" class="needs-validation" novalidate="">
+                                <form method="POST" action="{{ route('login.process') }}">
                                     @csrf
                                     <div class="form-group">
                                         <label for="email">Email / NIS</label>
-                                        <input id="email" type="text" class="form-control" name="email" tabindex="1"
-                                            required autofocus>
+                                        <input id="email" type="text" class="form-control" name="email"
+                                            tabindex="1" required autofocus>
                                         <div class="invalid-feedback">
                                             Please fill in your email
                                         </div>
@@ -91,11 +91,11 @@
     <!-- General JS Scripts -->
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"></script>
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"></script>
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
     <script src="{{ asset('dashboard/assets/js/stisla.js') }}"></script>
