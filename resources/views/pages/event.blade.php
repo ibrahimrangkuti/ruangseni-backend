d1bb0babf8abd5c89bd631d26d9b297a1649b22d
ibrahimrangkuti <rangkutyibrahim765@gmail.com>
2022-11-21 21:02:10 +0700

    merapihkan halaman leaderboard'

--- resources/views/pages/event.blade.php
+++ resources/views/pages/event.blade.php
@@ -11,7 +11,8 @@
                 <div class="col-lg-6 mx-auto text-center">
 
                     <h1 class="heading" data-aos="fade-up">Event</h1>
-                    <p class="mb-4" data-aos="fade-up">Yuk ikuti event ini, kembangkan kreatifitasmu dan jadilah yang terbaik.</p>
+                    <p class="mb-4" data-aos="fade-up">Yuk ikuti event ini, kembangkan kreatifitasmu dan jadilah yang
+                        terbaik.</p>
                 </div>
             </div>
         </div>
--- resources/views/pages/leaderboard.blade.php
+++ resources/views/pages/leaderboard.blade.php
@@ -3,15 +3,17 @@
 @section('content')
     <div class="hero overlay">
 
-    <div class="img-bg rellax">
-        <img src="{{ asset('assets/images/hero_4.jpg') }}" alt="Image" class="img-fluid">
-    </div>
-    <div class="container">
-        <div class="row align-items-center justify-content-start">
-            <div class="col-lg-6 mx-auto text-center">
+        <div class="img-bg rellax">
+            <img src="{{ asset('assets/images/hero_4.jpg') }}" alt="Image" class="img-fluid">
+        </div>
+        <div class="container">
+            <div class="row align-items-center justify-content-start">
+                <div class="col-lg-6 mx-auto text-center">
 
-                <h1 class="heading" data-aos="fade-up">Leaderboard</h1>
-                <p class="mb-4" data-aos="fade-up">Jadilah yang terbaik di antara yang terbaik.</p>
+                    <h1 class="heading" data-aos="fade-up">Leaderboard</h1>
+                    <p class="mb-4" data-aos="fade-up">Yuk ikuti event ini, kembangkan kreatifitasmu dan jadilah yang
+                        terbaik.</p>
+                </div>
             </div>
         </div>
 
@@ -25,29 +27,39 @@
 
 
             @foreach ($posts as $post)
-                <div class="row align-items-stretch mb-5">
-                    <div class="col-12">
+                <div class="d-flex row justify-content-between mb-4">
+
+                    <div class="col-lg-12">
                         <div class="card">
+                            <span class="bg-primary"
+                                style="border-radius: 100%;  position: absolute; padding: 20px 26px; z-index: 100; color: white">{{ $loop->iteration }}</span>
                             <div class="card-body">
                                 <div class="row">
-                                    <div class="col-md-3" style="max-height: 240px; overflow: hidden;">
-                                        <img src="{{ $post->img_url }}" alt="" class="img-fluid">
+                                    <div class="col-md-3">
+                                        <img src="{{ asset($post->img_url) }}" alt=""
+                                            class="shadow rounded img-fluid">
                                     </div>
-                                    <div class="col-md-9 mt-3">
-                                        <h2 class="fw-bold">{{ $post->title }}</h2>
-                                        <p>{{ $post->user->name }} | {{ $post->user->class }}</p>
-                                        <p>{!! $post->body !!}</p>
-                                        <a href="" class="btn btn-primary btn-sm">Detail Karya</a>
+                                    <div class="col-md-6 mt-5">
+                                        <h2>{{ $post->title }}</h2>
+                                        <p>{!! Str::limit($post->body, 400) !!}</p>
                                     </div>
-
                                 </div>
                             </div>
                         </div>
                     </div>
 
+
+                    {{-- <nav class="mt-5" aria-label="Page navigation example" data-aos="fade-up" data-aos-delay="100">
+                <ul class="custom-pagination pagination">
+                    <li class="page-item prev"><a class="page-link" href="#">Previous</a></li>
+                    <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item next"><a class="page-link" href="#">Next</a></li>
+                </ul>
+            </nav> --}}
                 </div>
             @endforeach
         </div>
     </div>
-</div>
 @endsection
